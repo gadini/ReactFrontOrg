a473d423a21719eb369cd2e31d38ba294930788d
gustavo.gadini <gustavo.gadini@redeflex.com.br>
2025-09-19 15:24:35 -0300

    Adicionando evento do botao

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -6,12 +6,18 @@ import './Form.css'
 const Form = () => {
 
     const times = [ 'Back-End', 'Front-End', 'Dados', 'Segurança']
+
+    const onSaving = (event) => {
+        event.preventDefault()
+        console.log("Form submetido com sucesso")
+    }
+
     return (
         <section className='form-field'>
-            <form>
+            <form onSubmit={onSaving} >
                 <h2>Preencha os dados para cadastrar colaborador</h2>
-                <TextField label='Nome' placeholder='Digite o seu nome' />
-                <TextField label='Cargo' placeholder='Digite o seu cargo' />
+                <TextField mandatory={true} label='Nome' placeholder='Digite o seu nome' />
+                <TextField mandatory={true} label='Cargo' placeholder='Digite o seu cargo' />
                 <TextField label='Imagem' placeholder='Digite o endereço da imagem'/>
                 <DropDown label='Time' items={times} />
                 <ButtonField text='Cadastrar' />
