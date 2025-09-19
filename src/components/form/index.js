f865249fc8cc48014700677e7f5962b15c7fb046
gustavo.gadini <gustavo.gadini@redeflex.com.br>
2025-09-19 14:53:21 -0300

    Alterando input time para select

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,15 +1,18 @@
+import DropDown from '../dropDown'
 import TextField from '../textField'
 import './Form.css'
 
 const Form = () => {
+
+    const times = [ 'Back-End', 'Front-End', 'Dados', 'Segurança']
     return (
         <section className='form-field'>
             <form>
-                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <h2>Preencha os dados para cadastrar colaborador</h2>
                 <TextField label='Nome' placeholder='Digite o seu nome' />
                 <TextField label='Cargo' placeholder='Digite o seu cargo' />
                 <TextField label='Imagem' placeholder='Digite o endereço da imagem'/>
-                <TextField label='Time' placeholder='Digite o seu time' />
+                <DropDown label='Time' items={times} />
             </form>
         </section>
     )
